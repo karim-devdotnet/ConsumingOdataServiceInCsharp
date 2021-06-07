89280a01091e16ff0828234361849b7eb2ac89a4
Abdelkarim Abdallah <abdelkarim.abdallah@brunata-hamburg.de>
2021-06-07 19:24:35 +0200

    OdataServiceClient for local OData service added

---
+++ src/OdataServiceClient/OdataServiceClient.Local.Konsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OdataServiceServer.Models;
+
+namespace OdataServiceClient.Local.Konsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListBooks().Wait();
+        }
+
+        static async Task ListBooks()
+        {
+            var serviceRoot = "https://localhost:44326/odata/";
+            var context = new Default.Container(new Uri(serviceRoot));
+
+            IEnumerable<Book> books = await context.Books.ExecuteAsync();
+            foreach (var book in books)
+            {
+                Console.WriteLine("{0} {1}", book.Title, book.ISBN);
+            }
+        }
+    }
+}
