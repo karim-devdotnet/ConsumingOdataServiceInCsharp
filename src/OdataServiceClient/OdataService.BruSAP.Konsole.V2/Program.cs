78b918074db1202ec44b608ac6fa18b62d48e6e9
Abdelkarim Abdallah <abdelkarim.abdallah@brunata-hamburg.de>
2021-06-24 17:58:57 +0200

    Variante 2: Twa_Auftrag OdataService.BruSAP.Konsole.V2  added

---
+++ src/OdataServiceClient/OdataService.BruSAP.Konsole.V2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OdataService.BruSAP.Konsole.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
